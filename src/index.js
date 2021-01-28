1fff88b932f79f57df25fcd80ec37aafe8b2c019
hakozaru <t.morii@everyleaf.com>
2021-01-28 14:47:37 +0900

    演習1-6終了

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h2>give feedback</h2>
+      <div>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>netural</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
