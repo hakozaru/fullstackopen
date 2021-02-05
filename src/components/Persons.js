f10e5fa6fbaf48781dea7c64bd9c00c225a895cd
hakozaru <t.morii@everyleaf.com>
2021-02-05 11:07:26 +0900

    演習2-20終了

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { deletePerson } from '../services/persons'
+import FlashMessage from './FlashMessage'
 
 const Persons = (props) => {
   const { persons, setPersons } = props
+  const defaultFlashObj = { message: null, error: false }
+  const [flashMsg, setFlashMsg] = useState(defaultFlashObj)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFlashMsg(defaultFlashObj)
+    }, 5000)
+  }, [flashMsg])
 
   const removePerson = (person) => {
     if(window.confirm(`Delete ${person.name} ?`)) {
       deletePerson(person.id)
       .then(_ => {
         setPersons(persons.filter(p => p.id !== person.id))
-        alert("delete successful.")
+        setFlashMsg({
+          message: `Delete successful.`,
+          error: false
+        })
       })
-      .catch(_ => alert("delete failed."))
+      .catch(_ => {
+        setFlashMsg({
+          message: `Information of ${person.name} has already been removed from server`,
+          error: true
+        })
+      })
     }
   }
 
   return(
     <>
+      <FlashMessage flashMessage={flashMsg} />
       {persons.map((p) => {
         return(
           <div key={p.name}>
